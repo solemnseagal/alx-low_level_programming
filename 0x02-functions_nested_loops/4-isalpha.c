ee24c22c441d47d799ad0fd46b317baa3f4adebd
David Echoda <Davidechoda123@gmail.com>
2021-06-22 13:06:02 -0700

    checking for lowercase c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* 4-isalpha.c - Print the alphabet in lowercase.
+*
+* Return: 0 if variable c is not a letter or 1 if c is a letter, uppercase or lowercase.
+*/
+int 4-isalpha.c(int c)
+{
+if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
