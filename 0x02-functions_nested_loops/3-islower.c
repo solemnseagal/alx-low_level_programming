efa8f2a7389591cdaa50a9a4639e2f262311542e
David Echoda <Davidechoda123@gmail.com>
2021-06-22 12:40:27 -0700

    checking for lowercase c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* print_alphabet_x10 - Print the alphabet in lowercase.
+*
+* Return: void.
+*/
+int _islower(int c)
+{
+if (c == 'c')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
+
