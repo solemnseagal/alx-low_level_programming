e5070776ff3f0584084af29f58628b2da3ae81d2
David Echoda <Davidechoda123@gmail.com>
2021-06-24 09:43:12 -0700

    printing digits

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -11,7 +11,7 @@ void print_numbers(void)
 char num;
 for (num = '0'; num <= '9'; num++)
 {
-_putchar (num + '0');
+_putchar (num);
 }
 _putchar ('\n');
 return (0);
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,7 +12,7 @@ for (num = '0'; num <= '9'; num++)
 {
 if ((num != '2') && (num != '4'))
 {
-_putchar (num + '0');
+_putchar (num);
 }
 }
 _putchar ('\n');
