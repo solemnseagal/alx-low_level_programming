09ac931b67de49e0ee709b4e26428129fd7d7a87
David Echoda <Davidechoda123@gmail.com>
2021-06-24 09:51:36 -0700

    printing digits

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * print_numbers -  print numbers from 0 to 9 except 2 and 4.
+ * print_most_numbers -  print numbers from 0 to 9 except 2 and 4.
  *
  * Return: Always 0.
  */
