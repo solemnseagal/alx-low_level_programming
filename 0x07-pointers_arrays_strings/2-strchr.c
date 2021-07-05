fe39a52580acda581181b000e234d1dff5b2c176
David Echoda <Davidechoda123@gmail.com>
2021-07-05 13:11:51 -0700

    correction to code

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-
+/**
+ * _strchr - Locates a character in a string
+ * @s: String
+ * @c: Character to search
+ * Return: Pointer to the first occurrence of the character c in the string s
+ **/
 char *_strchr(char *s, char c)
 {
-while(*s == '\0')
+while (*s == '\0')
 {
 if (*s == c)
 {
