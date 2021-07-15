406062ae077e9fcadde21ac580aa6e8e73250d7e
David Echoda <Davidechoda123@gmail.com>
2021-07-15 14:43:29 -0700

    code correction

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,20 +10,21 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *strDup;
-	int i;
-	unsigned int j;
+	int i = 0;
+	unsigned int j = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	i = 0;
+
 	while (s1[i] != '\0')
 		i++;
 	strDup = malloc(sizeof(char) * (i + n + 1));
 	if (strDup == NULL)
 		return (NULL);
-	i = j = 0;
+	i = 0;
+
 	while (s1[i] != '\0')
 	{
 		strDup[i] = s1[i];
