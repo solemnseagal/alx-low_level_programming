f1c858c352f8519b321d3bfa1b8fe5611ffd3678
David Echoda <Davidechoda123@gmail.com>
2021-07-21 09:40:22 -0700

    Coreection to code

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,8 @@
 #ifndef HEADERGUARD
 #define HEADERGUARD
+
+#include <stddef.h>
+#include <stdio.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
